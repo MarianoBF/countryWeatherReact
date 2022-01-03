4cb0ce933d3f4ec06af4d2d492e83b7b279385c0
Mariano <mariano.b.fernandez@gmail.com>
2022-01-03 17:48:31 -0300

    update sidebar with routes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,18 +1,18 @@
-function Sidebar() {
-    return (
-      <div>
-      
-      <h2>Buscar</h2>
-      <ul class="list-group">
-          <li class="list-group-item">Buscar por región</li>
-          <li class="list-group-item">Buscar por capital </li>
-          <li class="list-group-item">Buscar país</li>
-          <li class="list-group-item">Buscar limítrofes </li>
-          <li class="list-group-item">Listar banderas </li>
-      </ul>
+import Nav from "react-bootstrap/Nav";
 
-      </div>
-    );
-  }
-  
-  export default Sidebar;
+function Sidebar() {
+  return (
+    <div>
+      <h2>Buscar</h2>
+      <Nav variant="pills" defaultActiveKey="/" className="flex-column">
+        <Nav.Link eventKey="link-0" href="/">Buscar por región</Nav.Link>
+        <Nav.Link eventKey="link-1" href="/capital">Buscar por capital</Nav.Link>
+        <Nav.Link eventKey="link-2" href="/limitrofes">Buscar limítrofes</Nav.Link>
+        <Nav.Link eventKey="link-3" href="/pais">Buscar país</Nav.Link>
+        <Nav.Link eventKey="link-4" href="/banderas">Listar banderas</Nav.Link>
+      </Nav>
+    </div>
+  );
+}
+
+export default Sidebar;
