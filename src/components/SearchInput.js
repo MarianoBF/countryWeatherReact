7310914b62925b2ca1eb79703d4a0c0da6359a85
Mariano <mariano.b.fernandez@gmail.com>
2022-01-03 21:10:34 -0300

    add search by capital

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,14 +1,28 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
+import { useState } from "react";
 
-function SearchInput() {
+
+function SearchInput({handleSearch}) {
+
+  const [searchTerm, setSearchTerm] = useState();
+
+  const handleInput = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    handleSearch(searchTerm)
+  };
+
     return (
       <div>
-      <Form>
+      <Form onSubmit={handleForm}>
         <Form.Group className="mb-3">
           <Form.Label>Buscar orden:</Form.Label>
-          <Form.Control type="text" />
+          <Form.Control type="text" onChange={handleInput}/>
         </Form.Group>
         <Button type="submit">Buscar</Button>
       </Form>
--- src/pages/FindCapital.js
+++ src/pages/FindCapital.js
@@ -1,11 +1,33 @@
-function FindCapital() {
-    return (
-      <div>
-      
-Find Capital
+import { useState, useEffect } from "react";
 
-      </div>
-    );
+import CountryService from "../services/CountryService";
+import ResultsTable from "../components/ResultsTable";
+import SearchInput from '../components/SearchInput';
+
+function FindCapital() {
+  const [countryData, setCountryData] = useState([]);
+
+  const  [capital, setCapital] = useState(null);
+
+  useEffect(() => {
+    if (capital) {
+      CountryService.searchCountryByCapital(capital).then((res) =>
+        setCountryData(res.data)
+      );
+    }
+  }, [capital]);
+
+  const handleSearch = (search) => {
+    setCapital(search)
+  }
+
+  return (
+    <div className="App">
+      <h1>Buscar país por capital</h1>
+      <SearchInput handleSearch={handleSearch}/>
+      <ResultsTable seedData={countryData} />
+    </div>
+  );
   }
   
   export default FindCapital;
