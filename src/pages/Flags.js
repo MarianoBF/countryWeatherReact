2a2f04c7e7ae32d0c58722a45fef60e6e3ca48d6
Mariano <mariano.b.fernandez@gmail.com>
2022-01-05 17:46:31 -0300

    add flag listing page

--- src/pages/Flags.js
+++ src/pages/Flags.js
@@ -1,11 +1,34 @@
-function Flags() {
-    return (
-      <div>
-      
-Find banderas
+import { useEffect, useState } from "react";
+import Loading from "../components/Loading";
+import CountryService from "../services/CountryService";
 
-      </div>
-    );
+function Flags() {
+  const [countryData, setCountryData] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    CountryService.getAllCountries().then((res) => {
+      setCountryData(
+        res.data.sort((a, b) => (a.name.common > b.name.common ? 1 : -1))
+      );
+      setLoading(false);
+    });
+  }, []);
+
+  const flags = countryData.map((item) => (
+    <div key={item.cca3}>
+      <img src={item.flags.png} className="smallFlag" alt="flag" />
+      <p>{item.name.common}</p>
+    </div>
+  ));
+
+  if (loading) {
+    return <Loading />;
   }
-  
-  export default Flags;
+
+  return <div className="flagsGrid">{flags}</div>;
+}
+
+export default Flags;
