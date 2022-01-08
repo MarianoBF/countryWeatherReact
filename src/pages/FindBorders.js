aab9c683bf6998c292eca9d30e9b262ac5e101d2
Mariano <mariano.b.fernandez@gmail.com>
2022-01-08 10:12:46 -0300

    partial find borders

--- src/pages/FindBorders.js
+++ src/pages/FindBorders.js
@@ -1,11 +1,61 @@
-function FindBorders() {
-    return (
-      <div>
-      
-Find borders
+import Form from "react-bootstrap/Form";
 
-      </div>
-    );
+import { useEffect, useState } from "react";
+
+import Loading from "../components/Loading";
+import CountryService from "../services/CountryService";
+
+function FindBorders() {
+  const [loading, setLoading] = useState(false);
+
+  const regions = ["africa", "americas", "asia", "europe", "oceania"];
+
+  const regionsList = regions.map(item=>(
+    <option key={item}>{item}</option>
+  ))
+
+  const [selectedRegion, setSelectedRegion] = useState(null);
+
+  const handleSelectRegion = (e) => {
+    setSelectedRegion(e.target.value);
+  };
+
+  const [regionCountryList, setRegionCountryList] = useState([]);
+
+  useEffect(() => {
+    if (selectedRegion) {
+      setLoading(true);
+      CountryService.getCountriesByRegion(selectedRegion).then((res) => {
+        setRegionCountryList(res.data.map(item=>(
+          <option key={item.cca3}>{item.name.common}</option>
+        )));
+        setLoading(false);
+      });
+    }
+  }, [selectedRegion]);
+
+  if (loading) {
+    return <Loading />;
   }
-  
-  export default FindBorders;
+
+  return (
+    <div>
+      <h1>Buscar limítrofes de un país</h1>
+      <Form.Group className="mb-4" controlId="formRegion">
+        <Form.Label>Región</Form.Label>
+        <Form.Select size="lg" onChange={handleSelectRegion} value={selectedRegion}>
+        <option></option>
+          {regionsList}
+        </Form.Select>
+      </Form.Group>
+      <Form.Group className="mb-4" controlId="formCountryList">
+        <Form.Label>País</Form.Label>
+        <Form.Select size="lg" onChange={null}>
+          {regionCountryList}
+        </Form.Select>
+      </Form.Group>
+    </div>
+  );
+}
+
+export default FindBorders;
