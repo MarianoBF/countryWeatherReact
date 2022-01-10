f232b58364ad5877dda436f2e2f79479e2ea9c44
Mariano <mariano.b.fernandez@gmail.com>
2022-01-10 10:52:30 -0300

    adjust sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,22 +1,12 @@
 import Nav from "react-bootstrap/Nav";
 import { LinkContainer } from "react-router-bootstrap";
-import { useState } from "react";
 
 function Sidebar() {
-  const [active, setActive] = useState("aa");
 
   return (
     <div>
-      <h3>Buscar {active}</h3>
-      <Nav
-        variant="tabs"
-        activeKey={active}
-        className="flex-column"
-        onSelect={(selectedKey) => {
-          console.log(selectedKey);
-          setActive(selectedKey);
-        }}
-      >
+      <h3>Buscar</h3>
+      <Nav variant="tabs" className="flex-column">
         <LinkContainer to="/region">
           <Nav.Link>Buscar por región</Nav.Link>
         </LinkContainer>
